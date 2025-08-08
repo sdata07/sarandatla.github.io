97b03b6da3b08f7b2e34d11dc36738db3096a8c2
sdata07 <sdataquantum@gmail.com>
2025-08-08 16:55:39 +0000

    Deploying to gh-pages from @ sdata07/sdata07.github.io@9dd6e76c7782b868ce444f0930921e3997869373 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ipl-toss-and-match-winner",
+          title: 'IPL Toss and Match Winner',
+          description: "A project that visualizes the IPL toss and match prediction winner over time.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
